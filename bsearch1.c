b42b8015db0219348d738f39191a73c16549b11a
Evgenii Kuzovlev <43378104+pathfinder177@users.noreply.github.com>
2024-01-09 11:07:54 +0100

    Updated bsearch1.c search func

--- bsearch1.c
+++ bsearch1.c
@@ -1,34 +1,18 @@
 #include <stdio.h>
 
 #define LIM 11
-#define X 11
+#define X 12
 
 int arr_length(int arr[]);
 int bs(int l, int i[]);
 
 int main(){
-    int i[LIM];
-    i[0] = 2;
-    i[1] = 3;
-    i[2] = 4;
-    i[3] = 5;
-    i[4] = 6;
-    i[5] = 7;
-    i[6] = 8;
-    i[7] = 9;
-    i[8] = 10;
-    i[9] = 11;
-    i[10] = '\0';
+    int i[LIM] = {2,3,4,5,6,7,8,9,10,11,'\0'};
 
     int l = arr_length(i);
 
     if (l > 1) {
-        if (X == (i[l - 1])) {
-            printf("%d\n", l - 1);
-        }
-        else {
-            printf("%d\n", bs(l, i));
-        }
+        printf("%d\n", bs(l, i));
     }
     else if ((l == 1) && (X == i[0])) {
         return 0;
@@ -53,28 +37,23 @@ int arr_length(int arr[])
 
 int bs(int l, int i[])
 {
-    int min = 0;
-    int max = l-1;
-    int mid = max / 2;
+    int min, max, mid;
+    min = 0;
+    max = l-1;
 
-    while (mid != 0) {
-        if (X == i[mid]) {
-            return mid;
+    while (min <= max) {
+        mid = (max + min) / 2;
+
+        if (X > i[mid]) {
+            min = mid + 1;
         }
-        else if (X > i[mid]) {
-            min = mid;
-            mid = (max + min) / 2;
+        else if(X < i[mid]) {
+            max = mid - 1;
         }
         else {
-            max = mid;
-            mid = (max - min) / 2;
+            return mid;
         }
     }
 
-    if (X == i[mid]) {
-        return mid;
-    }
-    else {
-        return -1;
-    }
+    return -1;
 }
