f53b06456dccda8dcc7cc423cf746042ad1c9230
Evgenii Kuzovlev <evgenii.kuzovlev@hellofresh.com>
2024-04-11 10:52:28 +0200

    [ds-binary-tree] added dfs search

--- binary_tree.c
+++ binary_tree.c
@@ -24,22 +24,55 @@ void tree_insert_left(struct Node*, int);
 struct Node* tree_get_right_child(struct Node*);
 void tree_insert_right(struct Node*, int);
 
-struct Node* tree_search(struct Node*, int);
 
 //actually not the part of the interface, part of learning
 void tree_inorder_traverse(struct Node*);
 void tree_preorder_traverse(struct Node*);
 void tree_postorder_traverse(struct Node*);
 
+struct Node* tree_dfs_search(struct Node*, int);
 
-struct Node* tree_create_node(int value) {
-  struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+struct Node* tree_dfs_search(struct Node* self, int value) {
+    /*
+    base case: value is found OR not
+    rec. step:
+        1. explore current node
+            if found: return value
+        2. if value is not in current node
+            a. recursive exploration of left subtree
+                if found: return value
+        3. if value is not in left subtree
+            a. recursive exploration of right subtree
+                if found: return value
 
-  new_node->data = value;
-  new_node->l_node = NULL;
-  new_node->r_node = NULL;
+        if not found: return NULL
+    final state:
+        1. value is returned. algorithm are stopped.
+        2. NULL is returned. all nodes are visited
+    */
 
-  return new_node;
+   struct Node* n = NULL;
+
+    if(self->data == value) {
+        n = self;
+        return n;
+    }
+
+    if(self->l_node) {
+        n = tree_dfs_search(self->l_node, value);
+        if((n) && n->data == value) {
+            return n;
+        }
+    }
+
+    if(self->r_node) {
+        n = tree_dfs_search(self->r_node, value);
+        if((n) && n->data == value) {
+            return n;
+        }
+    }
+
+    return NULL;
 }
 
 void tree_postorder_traverse(struct Node* self) {
@@ -97,6 +130,17 @@ void tree_inorder_traverse(struct Node* self) {
     }
 }
 
+struct Node* tree_create_node(int value) {
+  struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+
+  new_node->data = value;
+  new_node->l_node = NULL;
+  new_node->r_node = NULL;
+
+  return new_node;
+}
+
+
 Tree tree_create() {
     Tree tree;
     tree.root = NULL;
@@ -121,7 +165,9 @@ int main() {
 
     //use DFS
     //backtracking problem
-    struct Node* n = tree_search(tree.root, 5);
+    struct Node* n = tree_dfs_search(tree.root, 7);
+
+
 
     // 4,2,5,1,6,3,7,
     // tree_inorder_traverse(tree.root);
