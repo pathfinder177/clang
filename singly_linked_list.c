3a22e0defb6cc6d89b1b8eabed2fa3fe0a27220f
Evgenii Kuzovlev <evgenii.kuzovlev@hellofresh.com>
2024-04-19 17:23:39 +0200

    [ds-singly-ll] errors to stderr: replaced printf with fputs

--- singly_linked_list.c
+++ singly_linked_list.c
@@ -27,7 +27,7 @@ void list_splice_after(List* self, struct Node* after, List* other);
 
 void list_splice_after(List* self, struct Node* after, List* other) {
     if (self -> head == NULL && other -> head == NULL) {
-        printf("Can not SPLICE_AFTER: both lists are empty\n");
+        fputs("Can not SPLICE_AFTER: both lists are empty\n", stderr);
         exit(EXIT_FAILURE);
     }
 
@@ -50,7 +50,7 @@ void list_splice_after(List* self, struct Node* after, List* other) {
 
 void list_merge_to_front(List* self, List* other) {
     if (self -> head == NULL && other -> head == NULL) {
-        printf("Can not MERGE_TO_FRONT: both lists are empty\n");
+        fputs("Can not MERGE_TO_FRONT: both lists are empty\n", stderr);
         exit(EXIT_FAILURE);
     }
 
@@ -66,7 +66,7 @@ void list_append(List* self, int value) {
     struct Node *p_new_node = (struct Node *) malloc(sizeof(struct Node));
 
     if (p_new_node == NULL) {
-        printf("Memory allocation failed for Node\n");
+        fputs("Memory allocation failed for Node\n", stderr);
         exit(EXIT_FAILURE);
     }
 
@@ -83,7 +83,7 @@ void list_append(List* self, int value) {
 
 void list_merge_to_back(List* self, List* other) {
     if (self -> head == NULL && other -> head == NULL) {
-        printf("Can not MERGE_TO_BACK: both lists are empty\n");
+        fputs("Can not MERGE_TO_BACK: both lists are empty\n", stderr);
         exit(EXIT_FAILURE);
     }
 
@@ -96,11 +96,11 @@ void list_merge_to_back(List* self, List* other) {
 
 void list_erase_after(List* self, struct Node* node) {
     if (self -> head == NULL) {
-        printf("Can not ERASE_AFTER: list is empty\n");
+        fputs("Can not ERASE_AFTER: list is empty\n", stderr);
         exit(EXIT_FAILURE);
     }
     else if(node -> next == NULL) {
-        printf("Can not ERASE_AFTER: your node is the last in list\n");
+        fputs("Can not ERASE_AFTER: your node is the last in list\n", stderr);
         exit(EXIT_FAILURE);
     }
 
@@ -113,7 +113,7 @@ void list_erase_after(List* self, struct Node* node) {
 
 struct Node* list_find(List* self, int value) {
     if (self -> head == NULL) {
-        printf("Can not FIND: list is empty\n");
+        fputs("Can not FIND: list is empty\n", stderr);
         exit(EXIT_FAILURE);
     }
 
@@ -131,7 +131,7 @@ struct Node* list_find(List* self, int value) {
 
 int list_pop_front(List* self) {
     if (self -> head == NULL) {
-        printf("Can not POP: list is empty\n");
+        fputs("Can not POP: list is empty\n", stderr);
         exit(EXIT_FAILURE);
     }
 
@@ -161,7 +161,7 @@ void list_push_front(List* self, int value) {
     struct Node *p_new_node;
 
     if ((p_new_node = (struct Node *) malloc(sizeof(struct Node))) == NULL) {
-        printf("Memory allocation failed for Node\n");
+        fputs("Memory allocation failed for Node\n", stderr);
         exit(EXIT_FAILURE);
     }
 
