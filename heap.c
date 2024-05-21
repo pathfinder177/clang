530864b350c2d9fe2108852525c6ac27a74a1869
Evgenii Kuzovlev <evgenii.kuzovlev@hellofresh.com>
2024-05-21 13:23:52 +0200

    [ds-heap] began to change interface

--- heap.c
+++ heap.c
@@ -1,99 +1,130 @@
+//Binary MAX heap implementation
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include "node.h"
 
-//Binary MAX heap implementation
 
-// typedef struct {
-    // int values[];
-// } Heap;
+#define INIT_HEAP_SIZE 10
+#define HEAP_SIZE_CHANGE 5
+#define MAX_HEAP_SIZE 100
 
-void heap_make_from_array(int[], int); //fill heap with array's values
-bool heap_erase_value_from_array(int[], int, int); //fill heap with array's values
+typedef struct {
+    int size;
+    int last_elem_pos;
+    int *array;
+} Heap;
 
-// void heap_push(int[], int);
-// int heap_pop(int[]);
-// int heap_peek(int[]);
+Heap heap_create();
+void heap_free(Heap*);
+static void heap_realloc(Heap*);
 
-void heap_make_from_array(int heap_values[], int heap_size) {
-    int i, index, parent, tmp;
+void heap_push(Heap*, int);
 
-    for(i = 0; i < heap_size; i++) {
-        index = i;
+void heap_print(Heap*);
 
-        while (index != 0) {
-            parent = (index - 1) / 2;
-
-            if (heap_values[index] <= heap_values[parent]) {
-                break;
-            }
-
-            tmp = heap_values[index];
-            heap_values[index] = heap_values[parent];
-            heap_values[parent] = tmp;
-
-            index = parent;
-        }
+void heap_print(Heap* heap) {
+    int i;
+    if(heap->last_elem_pos < 0) {
+        fprintf(stderr, "Can not prinf, heap is empty\n");
+        abort();
     }
+
+    for(i = 0; i <= heap->last_elem_pos; i++) {
+        printf("%d ", heap->array[i]);
+    }
+    printf("\n");
 }
 
-bool heap_erase_value_from_array(int heap_values[], int heap_size, int value) {
-    int i, left_child, right_child, parent, tmp;
+Heap heap_create() {
+    Heap heap;
 
-    for (i = 0; i < heap_size; i++) {
-        if(heap_values[i] == value) {
-            parent = i;
-            break;
-        }
+    heap.array = (int *)calloc(heap.size, sizeof(int)); //to init with 0
+
+    if (heap.array == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        abort();
     }
 
-    if (parent < 0 || parent >= heap_size || heap_values[parent] != value) {
-        printf("No %d in heap\n", value);
-        return false;
+    heap.size = INIT_HEAP_SIZE;
+    heap.last_elem_pos = -1;
+
+    return heap;
+}
+
+void heap_free(Heap* heap) {
+    free(heap->array);
+
+    heap->array = NULL; //avoid undefined behavior of dangling pointer
+    heap->last_elem_pos = -1;
+    heap->size = INIT_HEAP_SIZE;
+}
+
+static void heap_realloc(Heap* heap) {
+    if (heap->size >= MAX_HEAP_SIZE) {
+        fprintf(stderr, "Heap exceeds max size\n");
+        abort();
     }
 
-    heap_values[parent] = heap_values[heap_size-1];
+    int new_heap_size = heap->size + HEAP_SIZE_CHANGE;
+    int *tmp = (int *)realloc(heap->array, new_heap_size*sizeof(int));
 
-    left_child = 2 * parent + 1;
-    right_child = 2 * parent + 2;
-
-    while(left_child < heap_size - 1) {
-        if(heap_values[left_child] < heap_values[parent] && heap_values[right_child] < heap_values[parent]) {
-            break;
-        }
-
-        if(heap_values[left_child] > heap_values[right_child]) {
-            tmp = heap_values[parent];
-            heap_values[parent] = heap_values[left_child];
-            heap_values[left_child] = tmp;
-
-            parent = left_child;
-        }
-        else {
-            tmp = heap_values[parent];
-            heap_values[parent] = heap_values[right_child];
-            heap_values[right_child] = tmp;
-
-            parent = right_child;
-        }
-
-        left_child = 2 * parent + 1;
-        right_child = 2 * parent + 2;
+    if (tmp == NULL) {
+        fprintf(stderr, "Reallocation to increase size failed\n");
+        abort();
     }
 
-    return true;
+    heap->size = new_heap_size;
+    heap->array = tmp;
+}
+
+void heap_push(Heap* heap, int value) {
+    if(heap->array[0] == 0) { //heap is empty
+        heap->array[0] = value;
+        heap->last_elem_pos = 0;
+    }
+    else {
+        //TODO check whether heap is full and need additional memory
+
+        int parent = (heap->last_elem_pos - 1) / 2;
+        int left_child = 2 * parent + 1;
+        int right_child = 2 * parent + 2;
+
+        if(value <= heap->array[parent]) { //insert in the end
+            if(heap->array[left_child] == 0) {
+                heap->array[left_child] = value;
+            }
+            else if(heap->array[right_child] == 0) {
+                heap->array[right_child] = value;
+            }
+        }
+        else { //swap with parent and keep binary heap properties
+            int tmp;
+
+            if(heap->array[left_child] == 0) {
+                heap->array[left_child] = heap->array[parent];
+            }
+            else if(heap->array[right_child] == 0) {
+                heap->array[right_child] = heap->array[parent];
+            }
+
+            heap->array[parent] = value;
+
+            heap->last_elem_pos++;
+        }
+    }
 }
 
 int main() {
 
-    // int heap_values[] = {15,11,10,7,5,9,2,6,4,3,1,8,12}; //FIXME data from Stevens book
+    int heap_values[] = {10,5,3,9,7,8,1,4,15,12,14,13,18};
 
-    int heap_values[] = {10,5,3,9,7,8,1,4,15,12,14,13,18}; //data from binary_search_tree.c
-    int heap_size = (sizeof(heap_values))/(sizeof(heap_values[0]));
+    Heap heap = heap_create();
 
-    heap_make_from_array(heap_values, heap_size);
-    bool is_erased = heap_erase_value_from_array(heap_values, heap_size, 14);
+    int i;
+    for(i=0; i < (sizeof(heap_values)/sizeof(heap_values[0])); i++) {
+        heap_push(&heap, heap_values[i]);
+    }
+
+    heap_free(&heap);
 
     return 0;
 }
