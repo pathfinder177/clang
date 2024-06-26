0c2b01472b4380bdee261eadf86fb19656e171fd
Evgenii Kuzovlev <evgenii.kuzovlev@hellofresh.com>
2024-06-26 16:49:00 +0200

    [ds-graph] use downsift in pop, upsift in push. made heapify apart static func

--- graph_djikstra_heap.c
+++ graph_djikstra_heap.c
@@ -44,13 +44,12 @@ int* heap_pop(Heap *self) {
     }
 
     int* popped_arr = self->p_queue[0];
+    self->p_queue[0] = self->p_queue[self->size - 1];
     self->size--;
 
     if (self->size <= 0) {
         return popped_arr;
     }
-
-    self->p_queue[0] = self->p_queue[self->size];
 
     int heap_size = self->size;
     int** heap_values = self->p_queue;
@@ -59,8 +58,9 @@ int* heap_pop(Heap *self) {
     index = 0;
 
     //As only one node may be move down, the only cycle is sufficient
+    //downsift
     while(1) {
-        heap_print(self);
+        // heap_print(self);
         left = 2 * index + 1;
         right = 2 * index + 2;
         smallest = index;
@@ -115,21 +115,52 @@ void heap_push(Heap *self, int* priority_key_arr) {
         heap_realloc(self);
     }
 
-    self->p_queue[self->size] = priority_key_arr;
     self->size++;
+    self->p_queue[self->size-1] = priority_key_arr;
 
-    if(self->size > 1) {
-        heap_heapify(self);
+    int heap_size = self->size;
+    int** heap_values = self->p_queue;
+
+    int index, left, right, smallest;
+    index = (heap_size - 2) / 2;
+
+    //As only one node may be move down, the only cycle is sufficient
+    //upsift
+    while(1) {
+        heap_print(self);
+        left = 2 * index + 1;
+        right = 2 * index + 2;
+        smallest = index;
+
+        if (left < heap_size && heap_values[left][0] < heap_values[smallest][0]) {
+            smallest = left;
+        }
+
+        if (right < heap_size && heap_values[right][0] < heap_values[smallest][0]) {
+            smallest = right;
+        }
+
+        if (smallest == index) {
+            break;
+        }
+
+        int *tmp = heap_values[index];
+        heap_values[index] = heap_values[smallest];
+        heap_values[smallest] = tmp;
+
+        index = smallest;
     }
+
 }
 
-void heap_heapify(Heap *self) {
+static void heap_heapify(Heap *self) {
     int heap_size = self->size;
     int** heap_values = self->p_queue;
 
     int i, index, left, right, smallest;
 
     // Start from the last non-leaf node to compare it to it's child(ren) and move upwards
+    // upsift
     for (i = (heap_size - 1) / 2; i >= 0; i--) {
         index = i;
         // printf("parent index distance val = %d\n", heap_values[index][0]);
